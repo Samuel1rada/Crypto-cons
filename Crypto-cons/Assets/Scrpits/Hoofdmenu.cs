22eb33246b0ccb1d2e27deee052bc83eb1126fbd
Samuel1rada <s.rada@student.alfa-college.nl>
2023-01-12 09:02:42 +0100

    Game+PuazeMenu

--- Crypto-cons/Assets/Scrpits/Hoofdmenu.cs
+++ Crypto-cons/Assets/Scrpits/Hoofdmenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Spelen : MonoBehaviour
+public class Hoofdmenu : MonoBehaviour
 {
     public void PlayGame()
     {
---
+++ Crypto-cons/Assets/Scrpits/Terug_hooftmenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Terug_hooftmenu : MonoBehaviour
+{
+    public void TerugHoofdMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
